89f5c0d11a2974ecbb084356f0a5147d4d95e3a3
Peter Knowles <KnowlesPJ@Cardiff.ac.uk>
2017-04-28 17:00:48 +0100

    Profiler can be constructed to sort results by name, wall or cpu

--- main.cpp
+++ main.cpp
@@ -13,22 +13,22 @@ int main(int argc, char *argv[])
 #ifdef MPI2
     MPI_Init(&argc,&argv);
 #endif
-    Profiler profiler("C++");
+    Profiler profiler("C++",Profiler::name);
 
     profiler.start("sqrt");
     double a=(double)0;
-    for (int i=0; i<repeat; i++) a*=std::sqrt(a+i)/std::sqrt(a+i+1);
+    for (size_t i=0; i<repeat; i++) a*=std::sqrt(a+i)/std::sqrt(a+i+1);
     profiler.stop("sqrt",2*repeat);
 
     profiler.start("exp");
-    for (int i=0; i<repeat; i++) a*=std::exp(a+(double)1/i)/std::exp(a+(double)1/i+1);
+    for (size_t i=0; i<repeat; i++) a*=std::exp(a+(double)1/i)/std::exp(a+(double)1/i+1);
     profiler.stop("exp",2*repeat);
 
-    profiler.start("getResources"); for (int i=0; i<repeatr; i++) struct Profiler::resources r = profiler.getResources(); profiler.stop("getResources",2*repeatr);
+    profiler.start("getResources"); for (size_t i=0; i<repeatr; i++) struct Profiler::resources r = profiler.getResources(); profiler.stop("getResources",2*repeatr);
 
-    for (int i=0; i<repeatr ; i++){ profiler.start("profiler"); profiler.stop("profiler",1); }
+    for (size_t i=0; i<repeatr ; i++){ profiler.start("profiler"); profiler.stop("profiler",1); }
 
-    profiler.start("gettimeofday"); for (int i=0; i<repeatr ; i++){ struct timeval time; gettimeofday(&time,NULL); } profiler.stop("gettimeofday",repeatr);
+    profiler.start("gettimeofday"); for (size_t i=0; i<repeatr ; i++){ struct timeval time; gettimeofday(&time,NULL); } profiler.stop("gettimeofday",repeatr);
 
     std::cout << profiler << std::endl;
   }
@@ -37,12 +37,12 @@ int main(int argc, char *argv[])
     void* profilerC=profilerNew((char*)"C");
     double a=(double)0;
     profilerStart(profilerC,(char*)"sqrt");
-    for (int i=0; i<repeat; i++) a*=std::sqrt(a+i)/std::sqrt(a+i+1);
+    for (size_t i=0; i<repeat; i++) a*=std::sqrt(a+i)/std::sqrt(a+i+1);
     profilerStop(profilerC,(char*)"sqrt",2*repeat);
     profilerStart(profilerC,(char*)"exp");
-    for (int i=0; i<repeat; i++) a*=std::exp(a+(double)1/i)/std::exp(a+(double)1/i+1);
+    for (size_t i=0; i<repeat; i++) a*=std::exp(a+(double)1/i)/std::exp(a+(double)1/i+1);
     profilerStop(profilerC,(char*)"exp",2*repeat);
-    for (int i=0; i<1000000; i++) {
+    for (size_t i=0; i<1000000; i++) {
       profilerStart(profilerC,(char*)"profiler");
       profilerStop(profilerC,(char*)"profiler",1);
     }
