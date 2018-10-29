b4563b0526469a856857bdf167da46f8b277c207
Marat <sibaev.marat@gmail.com>
2018-10-29 09:38:30 +0000

    Remove the test from the library

--- lib/ProfilerF.F90
+++ lib/ProfilerF.F90
@@ -255,28 +255,3 @@ SUBROUTINE profiler_module_test(printlevel)
 #endif
 END SUBROUTINE profiler_module_test
 
-#ifdef MAIN
-PROGRAM mainf
- use profilerf
- INTEGER :: level
- CHARACTER(len=8) :: buff='1'
- type(profiler) :: p
- IF (command_argument_count().gt.0) CALL get_command_argument(1,buff)
- READ(buff,'(I8)') level
- CALL profiler_module_test(level)
-
- p = Profiler('testing')
- call p%active(99,stopPrint=0)
- call execute_command_line('sleep 1')
- call p%start('a')
- call execute_command_line('sleep 1')
- call p%start('b')
- call execute_command_line('sleep 1')
- call p%stop('b',1)
- call execute_command_line('sleep 1')
- call p%stop('a',100)
- call p%print(6)
- call p%print(6,cumulative=.true.)
-
-END PROGRAM mainf
-#endif
