755b901f19c37ca3e924e655faad7cdde3f63418
Marat Sibaev <sibaev.marat@gmail.com>
2020-11-02 16:42:30 +1300

    move implementation of report to report.cpp

--- src/molpro/Profiler/Tree/report.cpp
+++ src/molpro/Profiler/Tree/report.cpp
@@ -1,1 +1,36 @@
 #include "report.h"
+namespace molpro {
+namespace profiler {
+namespace tree {
+
+std::string tree_path(const std::shared_ptr<Node<Counter>>& leaf) {
+  std::string path;
+  auto node = leaf;
+  while (node) {
+    path = node->name + "." + path;
+    node = node->parent;
+  }
+  path.erase(prev(path.end()));
+  return path;
+}
+
+void report(Profiler& prof) {
+  struct Compare {
+    bool operator()(const Counter& x, const Counter& y) const {
+      return x.get_wall().cumulative_time() > y.get_wall().cumulative_time();
+    }
+  };
+  auto paths = std::map<Counter, std::string, Compare>{};
+  extract_counters(prof.root, paths);
+  std::cout << "Profiler " << '"' << prof.description << '"' << " cumulative " << std::endl;
+  std::for_each(begin(paths), end(paths), [](decltype(paths)::value_type& node) {
+    std::cout << node.second;
+    std::cout << "    calls=" << node.first.get_call_count() << ", ";
+    std::cout << " wall=" << node.first.get_wall().cumulative_time();
+    std::cout << std::endl;
+  });
+  std::cout << std::endl;
+}
+} // namespace tree
+} // namespace profiler
+} // namespace molpro
--- src/molpro/Profiler/Tree/report.h
+++ src/molpro/Profiler/Tree/report.h
@@ -11,34 +11,8 @@ namespace molpro {
 namespace profiler {
 namespace tree {
 
-/*!
- * @brief Removes the first leaf from the tree and returns it
- * @param node root of the subtree
- * @return
- */
-std::shared_ptr<Node<Counter>> remove_first_leaf(std::shared_ptr<Node<Counter>>& node) {
-  auto leaf = node;
-  if (leaf->children.empty())
-    return nullptr;
-  while (!leaf->children.empty()) {
-    leaf = leaf->children.begin()->second;
-  }
-  auto it = leaf->parent->children.begin();
-  leaf->parent->children.erase(it);
-  return leaf;
-}
-
 //! Returns path to the leaf by joining names from the root to the leaf
-std::string tree_path(const std::shared_ptr<Node<Counter>>& leaf) {
-  std::string path;
-  auto node = leaf;
-  while (node) {
-    path = node->name + "." + path;
-    node = node->parent;
-  }
-  path.erase(prev(path.end()));
-  return path;
-}
+std::string tree_path(const std::shared_ptr<Node<Counter>>& leaf);
 
 //! Depth-first-search through the tree and store Counter and its tree_path
 template <class Compare>
@@ -49,26 +23,7 @@ void extract_counters(const std::shared_ptr<Node<Counter>>& node, std::map<Count
       [&paths](const typename decltype(node->children)::value_type& ch) { extract_counters(ch.second, paths); });
 }
 
-void report(Profiler& prof) {
-  struct Compare {
-    bool operator()(const Counter& x, const Counter& y) const {
-      return x.get_wall().cumulative_time() > y.get_wall().cumulative_time();
-    }
-  };
-  auto paths = std::map<Counter, std::string, Compare>{};
-  extract_counters(prof.root, paths);
-  //  while (auto leaf = remove_first_leaf(prof.root)) {
-  //    paths[leaf->counter] = tree_path(leaf);
-  //  }
-  std::cout << "Profiler " << '"' << prof.description << '"' << " cumulative " << std::endl;
-  std::for_each(begin(paths), end(paths), [](decltype(paths)::value_type& node) {
-    std::cout << node.second;
-    std::cout << "    calls=" << node.first.get_call_count() << ", ";
-    std::cout << " wall=" << node.first.get_wall().cumulative_time();
-    std::cout << std::endl;
-  });
-  std::cout << std::endl;
-}
+void report(Profiler& prof);
 
 } // namespace tree
 } // namespace profiler
