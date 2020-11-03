9f2eb53e46309c44c672a491631c5bfeab905a07
Marat Sibaev <sibaev.marat@gmail.com>
2020-11-03 22:08:11 +1300

    remove all traces of the old profiler

--- src/molpro/ProfilerSingle.cpp
+++
@@ -1,8 +0,0 @@
-#include "ProfilerSingle.h"
-
-namespace molpro {
-
-ProfilerSingle::profilers_t ProfilerSingle::m_profilers{};
-ProfilerSingle::key_t ProfilerSingle::default_key{};
-
-} // namespace molpro
