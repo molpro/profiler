b0546305b31cc83b0e36f9bc14e9cd431499f234
Rob Welch <hello@robwel.ch>
2021-07-07 18:23:05 +0100

    Refactored dotgraph writer to merge nodes with the same name (unfinished)

--- src/molpro/profiler/dotgraph.cpp
+++ src/molpro/profiler/dotgraph.cpp
@@ -4,6 +4,7 @@ namespace molpro {
 namespace profiler {
 namespace dotgraph {
 
+
 // todo: move this to report.cpp and tidy up write_timing (of which this is one part)
 std::string frequency(size_t n_op, double time){
   std::stringstream ss;
@@ -53,32 +54,96 @@ std::string make_arrow(std::string name_from, std::string name_to, double time, 
   return ss.str();
 }
 
-std::string make_dotgraph_contents(std::shared_ptr<Node<Counter>> root, double total_time, int hot[3], int cool[3],
-                                    double threshold){
-  std::stringstream ss;
+void combine_graph_entries(GraphEntry& entry1, GraphEntry& entry2){
+  if (entry1.entry_type != entry2.entry_type){
+    throw std::runtime_error("Cannot combine node and edge");
+  }
+  entry2.runtime += entry1.runtime;
+  entry2.calls += entry1.calls;
+  entry2.operations += entry1.operations;
+}
+
+void make_dotgraph_vec(std::shared_ptr<Node<Counter>> root, double total_time, double threshold,
+                /*out*/ std::vector<GraphEntry>& graph_entries){
   // get values
   auto opcount = root->counter.get_operation_count();
   auto call_count = root->counter.get_call_count();
   double time = root->counter.get_wall().cumulative_time();
-  // only write box if threshold is exceeded
-  if (time/total_time > threshold){
-    ss << make_box(root->name, time, total_time, call_count, opcount, hot, cool);
-  }
+  graph_entries.emplace_back( GraphEntry(node, root->name, time, call_count, total_time, opcount, "") );
   for (auto& child : root->children){
     auto child_node = child.second;
     double child_time = child_node->counter.get_wall().cumulative_time();
+    auto child_call_count = child_node->counter.get_call_count();
     // make arrows for all children of this box
-    if (child_time/total_time > threshold){
-        ss << make_arrow(root->name, child_node->name, child_time, total_time, call_count, hot, cool);
-        // recurse into children
-        ss << make_dotgraph_contents(child_node, total_time, hot, cool, threshold);
+      graph_entries.emplace_back(GraphEntry(edge, root->name, child_time, child_call_count, total_time, -1,
+                                  child_node->name));
+      // recurse into children
+      make_dotgraph_vec(child_node, total_time, threshold, graph_entries);
+  }
+}
+
+void merge_vec(std::vector<GraphEntry>& graph_entries){
+  std::vector<std::pair<std::string, int>> names;
+  int size = graph_entries.size();
+  for(int i = 0; i<size; i++) {
+    bool entry_is_node = graph_entries[i].entry_type == node;
+    bool name_in_names = false;
+    for (int j = 0; j != names.size(); j++){
+      if (graph_entries[i].name == names[j].first && entry_is_node){
+        // combine vectors
+        combine_graph_entries(graph_entries[i], graph_entries[names[j].second]);
+        graph_entries.erase(graph_entries.begin() + i);
+        // correct iteration now that the vector has changed size
+        --i;
+        size = graph_entries.size();
+        name_in_names = true;
+      }
+    }
+    // keep track of which names exist and their indices
+    if (!name_in_names && entry_is_node){ names.emplace_back(std::make_pair(graph_entries[i].name, i)); }
+  }
+}
+
+std::string get_graph_markup(std::vector<GraphEntry>& graph_entries, double total_time, int hot[3], int cool[3]){
+  std::stringstream ss;
+  for(int i = 0; i<graph_entries.size(); i++) {
+    if (graph_entries[i].entry_type == node){
+      ss << make_box(graph_entries[i].name, graph_entries[i].runtime, total_time, graph_entries[i].calls,
+                graph_entries[i].operations, hot, cool);
+    }
+    if (graph_entries[i].entry_type == edge){
+      ss << make_arrow(graph_entries[i].name, graph_entries[i].name_to, graph_entries[i].runtime, total_time,
+                        graph_entries[i].calls, hot, cool);
     }
   }
   return ss.str();
 }
 
+GraphEntry::GraphEntry(EntryType entry_type, std::string name, double runtime, int calls,
+            double total_time, int operations, std::string name_to) 
+            : entry_type(entry_type), name(name), runtime(runtime), calls(calls), name_to(name_to),
+            operations(operations)
+{ }
+
+// TODO: either incorporate this or remove it
+//std::pair<std::string, std::string> GraphEntry::get_colours(int hot[3], int cool[3], double total_time){
+//  std::string colour = blend_colours(this->runtime/total_time, hot, cool);
+//  std::string fontcolour;
+//  if (this->entry_type == node){
+//      fontcolour = "#ffffff";
+//  } else {
+//      fontcolour = colour;
+//  }
+//  return std::make_pair(colour, fontcolour);
+//}
+
 std::string make_dotgraph(std::shared_ptr<Node<Counter>> root, double total_time, int hot[3], int cool[3],
                             double threshold){
+  std::vector<GraphEntry> graph_entries;
+  make_dotgraph_vec(root, total_time, threshold, graph_entries);      
+  merge_vec(graph_entries);          
+  std::string graph_contents = get_graph_markup(graph_entries, total_time, hot, cool);
+  
   std::stringstream ss;
   // write header info
   ss << "digraph {\n\n";
@@ -86,10 +151,12 @@ std::string make_dotgraph(std::shared_ptr<Node<Counter>> root, double total_time
   ss << "node [fontcolor=white, fontname=Arial, height=0, shape=box, style=filled, width=0];\n";
   ss << "edge [fontname=Arial, labeldistance=4.00, penwidth=4.00];\n";
   ss << "\n";
-  ss << make_dotgraph_contents(root, total_time, hot, cool, threshold);
+  ss << graph_contents;
   ss << "\n}";
   return ss.str();
 }
+
+
 
 } // namespace dotgraph
 } // namespace profiler
--- src/molpro/profiler/dotgraph.h
+++ src/molpro/profiler/dotgraph.h
@@ -13,6 +13,28 @@ namespace molpro {
 namespace profiler {
 namespace dotgraph {
 
+enum EntryType{ node, edge };
+
+/*! Represents a node (box) or edge in a graphviz digraph. */
+class GraphEntry{
+
+  public:
+
+    EntryType entry_type; // whether the entry is a node or edge
+    std::string name; // name of the node (used for labels and connectivity)
+    double runtime; // runtime of the node/edge
+    int calls; // number of calls for the node/edge
+    std::string name_to; // if this is a edge, this is where it goes to
+    std::string fontcolour; // colour of the font for an edge
+    int operations; // number of operations in a node
+    
+    GraphEntry(EntryType entry_type, std::string name, double runtime, int calls,
+            double total_time, int operations = -1, std::string name_to = "" );
+
+    std::pair<std::string, std::string> get_colours(int hot[3], int cool[3], double total_time);
+
+};
+
 // This file contains functions related to the creation of graphviz .dot files from profiles. The interface can be found
 // in profiler::dotgraph.
 
@@ -61,18 +83,23 @@ std::string make_box(std::string name, double time, double total_time, size_t ca
 std::string make_arrow(std::string name_from, std::string name_to, double time, double total_time, size_t call_count,
                         int hot[3], int cool[3]);
 
+// TODO: write docs for these functions
+void combine_graph_entries(GraphEntry& entry1, GraphEntry& entry2);
+void merge_vec(std::vector<GraphEntry>& graph_entries);
+std::string get_graph_markup(std::vector<GraphEntry>& graph_entries, double total_time, int hot[3], int cool[3]);
+
   /*!
-   * @brief This creates graphviz markup for all of the arrows and boxes, but not global styles.
+   * @brief This populates a vector containing a GraphEntry for each profiler node. This is an intermediate data
+   * structure to make it easier to merge profiler nodes but retain their connectivity.
    * @param root the root of the tree of nodes as defined in node.h.
    * @param total_time the total time taken by the program, in seconds.
-   * @param hot an 8-bit colour, 0-255. The more time spent in this node, the hotter the colour will be.
-   * @param cool an 8-bit colour, 0-255.
    * @param threshold a value between 0 and 1, a ratio of the program's runtime. If the program spends less than this
    * value in a given function, the box will not be drawn.
-   * @return graphviz markup for the arrows and boxes.
+   * @param graph_entries a vector (most likely empty) that will be filled with GraphEntry objects.
+   * @return nothing, but populates graph_entries.
    */
-std::string make_dotgraph_contents(std::shared_ptr<Node<Counter>> root, double total_time, int hot[3], int cool[3],
-                                    double threshold);
+void make_dotgraph_vec(std::shared_ptr<Node<Counter>> root, double total_time, double threshold,
+                /*out*/ std::vector<GraphEntry>& graph_entries);
 
   /*!
    * @brief This creates the complete graphviz markup for the whole performance graph, including global styles. The
