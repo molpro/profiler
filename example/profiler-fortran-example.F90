50a30ce3baec1bdaeea0283cbc0ddeeecbc3c17e
Peter Knowles <KnowlesPJ@Cardiff.ac.uk>
2022-07-22 07:28:44 +0100

    test timing_molpro

--- example/profiler-fortran-example.F90
+++ example/profiler-fortran-example.F90
@@ -15,9 +15,9 @@ PROGRAM mainf
     END SUBROUTINE MPI_FINALIZE
   END INTERFACE
 #endif
-    INTEGER :: level, ierr
+    INTEGER :: level, ierr, repeat
     CHARACTER(len = 8) :: buff = '1'
-    type(profiler) :: p
+    type(profiler) :: p, pcpu
 #ifdef HAVE_MPI_H
     call MPI_Init(c_null_ptr, c_null_ptr)
     !call MPI_INIT(ierr)
@@ -37,10 +37,49 @@ PROGRAM mainf
     call p%stop('b', 1)
     call execute_command_line('sleep 1')
     call p%stop('a', 100)
+    repeat=100000
+    call p%start('many wall')
+    do i=1,repeat
+        call p%start('wall')
+        call p%stop('wall')
+    end do
+    call p%stop('many wall', repeat)
+    call p%start('many wall plus fortran cpu_time')
+    do i=1,repeat
+        call p%start('wall plus fortran cpu_time')
+        call cpu_time(t)
+        call p%stop('wall plus fortran cpu_time')
+    end do
+    call p%stop('many wall plus fortran cpu_time', repeat)
+    call p%start('many fortran cpu_time')
+    do i=1,repeat
+        call cpu_time(t)
+    end do
+    call p%stop('many fortran cpu_time', repeat)
     call p%print(6)
     call p%print(6, cumulative = .false.)
     call p%dotgraph('profiler-fortran-example.gv')
     call p%destroy() ! This has to be called only if p is declared in the main program, because in that case the final subroutine of Profiler is not invoked
+    pcpu = Profiler('wall+cpu', 1, 0, cpu=.true.)
+    call pcpu%active(99, stopPrint = 0)
+    call pcpu%start('many wall+cpu')
+    do i=1,repeat
+        call pcpu%start('wall+cpu')
+        call pcpu%stop('wall+cpu')
+    end do
+    call pcpu%stop('many wall+cpu', repeat)
+    call pcpu%start('many timing_molpro')
+    do i=1,repeat
+        call TIMING_MOLPRO(cpu, sys, wall)
+    end do
+    call pcpu%stop('many timing_molpro', repeat)
+    call pcpu%start('many walcl')
+    do i=1,repeat
+        a=wallcl()
+    end do
+    call pcpu%stop('many walcl', repeat)
+    call pcpu%print(6)
+    call pcpu%destroy() ! This has to be called only if p is declared in the main program, because in that case the final subroutine of Profiler is not invoked
 #ifdef HAVE_MPI_H
   call MPI_Finalize()
   !call MPI_FINALIZE(ierr)
